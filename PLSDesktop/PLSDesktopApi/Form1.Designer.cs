b4621069fcfbdb1a4c69ada4bdb325a99158c693
IliaValov <iliqvulov@abv.bg>
2019-03-08 11:47:17 +0200

    Adding desktopapp and updates on the server

---
+++ PLSDesktop/PLSDesktopApi/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PLSDesktopApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GetLocations";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(169, -1);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 100;
+            this.gMapControl1.MinZoom = 5;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(630, 439);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 2D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+      
+    }
+}
+
---
+++ PLSDesktop/PLSDesktopApi/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace PLSDesktopApi
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, List<double>> usersLocations = new Dictionary<string, List<double>>();
+        List<GMapMarker> gmapMarkers = new List<GMapMarker>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            GetInformation();
+
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.MapProvider = GMapProviders.BingHybridMap;
+            gMapControl1.Position = new PointLatLng(12, 16);
+            gMapControl1.MinZoom = 5;
+            gMapControl1.MaxZoom = 100;
+            gMapControl1.Zoom = 10;
+
+            GMapOverlay markers = new GMapOverlay("markers");
+
+            foreach (var userKey in usersLocations)
+            {
+                for (int i = 0; i < userKey.Value.Count(); i += 3)
+                {
+                    GMapMarker marker = new GMarkerGoogle(
+                                    new PointLatLng(userKey.Value[i], userKey.Value[i + 1]),
+                                    GMarkerGoogleType.blue_pushpin);
+                    gmapMarkers.Add(marker);
+                }
+            }
+
+            foreach (var item in gmapMarkers)
+            {
+                markers.Markers.Add(item);
+                gMapControl1.Overlays.Add(markers);
+                gMapControl1.ShowCenter = false;
+            }
+        }
+
+        private void GetInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+
+            string result = string.Empty;
+            string url = @"https://localhost:44301/api/values";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            var list = result.Split(new char[] { ' ', '"', '{', '}' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Console.WriteLine(result);
+
+
+            for (int i = 0; i < list.Count; i += 2)
+            {var list2 = list[i + 1].Split(new char[] { ',', '[', ']', ':' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                if (!usersLocations.ContainsKey(list[i]))
+                {
+                    usersLocations.Add(list[i], new List<double>());
+                }
+                else
+                {
+                    
+                    for (int j = 0; j < list2.Count; j += 3)
+                    {
+                        usersLocations[list[i]].Add(double.Parse(list2[j]));
+                        usersLocations[list[i]].Add(double.Parse(list2[j + 1]));
+                        usersLocations[list[i]].Add(double.Parse(list2[j + 2]));
+                    }
+                    return;
+                }
+
+                list[i + 1].Split(new char[] { ',', '[', ']', ':' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                for (int j = 0; j < list2.Count; j += 3)
+                {
+                    usersLocations[list[i]].Add(double.Parse(list2[j]));
+                    usersLocations[list[i]].Add(double.Parse(list2[j + 1]));
+                    usersLocations[list[i]].Add(double.Parse(list2[j + 2]));
+                }
+            }
+
+
+
+        }
+    }
+}
