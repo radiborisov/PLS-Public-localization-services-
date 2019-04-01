20e033616bc04101729be8ff1f7b90e35bb12ba5
IliaValov <iliqvulov@abv.bg>
2019-04-01 21:05:56 +0300

    Adding the new mobile app

--- PLSMobileApp/app/src/main/java/com/example/plsmobileapp/MainActivity.java
+++ PLSMobileApp/app/src/main/java/com/example/plsmobileapp/MainActivity.java
@@ -1,13 +1,110 @@
 package com.example.plsmobileapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
+
+    Activity activity;
+
+    GPSTracker mGPS;
+
+    LocationManager mLocationManager;
+
+    TextView longitude;
+    TextView latitude;
+    TextView altitude;
+
+    String phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.register_layout);
+
+        final EditText inputPhoneNumber = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+        context = this;
+        activity = this;
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                phoneNumber = inputPhoneNumber.getText().toString();
+
+                setContentView(R.layout.activity_main);
+
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+                mGPS = new GPSTracker(context);
+                longitude = (TextView) findViewById(R.id.textView);
+                latitude = (TextView) findViewById(R.id.textView2);
+                altitude = (TextView) findViewById(R.id.textView3);
+
+                mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60 * 1000,
+                        0, mLocationListener);
+            }
+        });
+
+
     }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            longitude.setText(location.getLongitude() + "");
+            latitude.setText(location.getLatitude() + "");
+            altitude.setText(location.getAltitude() + "");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+   // @Override
+   // public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+    //    getMenuInflater().inflate(R.menu.main, menu);
+     //   return true;
+   // }
+
 }
